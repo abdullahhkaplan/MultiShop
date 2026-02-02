959fc80683a0f2601b208d717cbd53d6c022cf5d
abdullahhkaplan <abdullahhkaplan@gmail.com>
2026-02-02 12:02:01 +0300

    Add project files.

---
+++ Service/Catalog/MultiShop.Catalog/Entities/ProductImage.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MultiShop.Catalog.Entities
+{
+    public class ProductImage
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string ProductImageID { get; set; }
+        public string ProductImage1 { get; set; }
+        public string ProductImage2 { get; set; }
+        public string ProductImage3 { get; set; }
+
+        public string ProductID { get; set; }
+        [BsonIgnore]
+        public Product Product { get; set; }
+
+    }
+}
