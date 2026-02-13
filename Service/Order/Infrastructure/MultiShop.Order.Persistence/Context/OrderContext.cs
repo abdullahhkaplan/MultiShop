24136501aa9599bab0cab8f6fc16b5bef6269c8b
abdullahhkaplan <abdullahhkaplan@gmail.com>
2026-02-13 14:53:03 +0300

    Cuma ikinci Commit

---
+++ Service/Order/Infrastructure/MultiShop.Order.Persistence/Context/OrderContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiShop.Order.Persistence.Context
+{
+    public class OrderContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+        public DbSet<Address> addresses { get; set; }
+        public DbSet<OrderDetail> orderDetails { get; set; }
+        public DbSet<Ordering> orderings { get; set; }
+
+    }
+}
