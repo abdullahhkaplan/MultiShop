33f219bbeb5c5dedfadddbefff9190da58e3ad8b
abdullahhkaplan <abdullahhkaplan@gmail.com>
2026-05-07 17:42:59 +0300

    ara sonrası

---
+++ Service/Order/Core/MultShop.Order.Application/Services/ServiceRegistiration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiShop.Order.Application.Services
+{
+    public static class ServiceRegistiration
+    {
+        public static void AddAplicationService()
+    }
+}
